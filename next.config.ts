dfa938750a983677e7a56b95aa9cf985595a2899
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-12-12 01:05:27 +0700

    k

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/custom-route",
+        destination: "/feature/user",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
